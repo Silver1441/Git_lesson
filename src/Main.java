f386e4edde23a9ae737baa47503967364e668896
Ivan <Kishkan91@gmail.com>
2019-09-24 17:14:45 +0400

    demo service mock is added

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,12 @@
 import sevice.DemoService;
 import sevice.DemoServiceImpl;
+import sevice.DemoServiceImplMock;
 
 public class Main {
 
     public static void main(String[] args) {
         String arg = "Test Action";
-        DemoService demoService = new DemoServiceImpl();
+        DemoService demoService = new DemoServiceImplMock();
 
         demoService.makeAction(arg);
     }
---
+++ src/sevice/DemoServiceImplMock.java
@@ -0,0 +1,7 @@
+package sevice;
+
+public class DemoServiceImplMock implements DemoService {
+    public void makeAction(String arg) {
+        System.out.println("This is a mock test");
+    }
+}
