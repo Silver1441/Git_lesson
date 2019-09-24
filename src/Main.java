eab706c9c5dd426e22dc4c46d8c5c1adbed1c820
Ivan <Kishkan91@gmail.com>
2019-09-24 18:52:43 +0400

    arg changed for dev branch

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,7 @@ import sevice.demo.DemoTestImpl;
 public class Main {
 
     public static void main(String[] args) {
-        String arg = "Test Action";
+        String arg = "Test Action in dev branch";
 
         DemoTest demo = new DemoTestImpl();
 
