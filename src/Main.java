748374af8514513ff2f8709696daf7c8d6ce3516
Ivan <Kishkan91@gmail.com>
2019-09-24 19:06:04 +0400

    neArgs are added for demofeature

--- src/Main.java
+++ src/Main.java
@@ -5,9 +5,11 @@ public class Main {
 
     public static void main(String[] args) {
         String arg = "Test Action";
+        String newArg = "Testing new args";
 
         DemoTest demo = new DemoTestImpl();
 
         demo.makeTest(arg);
+        demo.makeTest(newArg);
     }
 }
