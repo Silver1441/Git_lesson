a9be679ee173dbf1050ce4d696af5c9534478c05
Ivan <Kishkan91@gmail.com>
2019-09-25 08:49:48 +0400

    dev branch bug solved

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,7 @@ import sevice.demo.DemoTestImpl;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("LOG: start");
+        System.out.println("LOG: test start");
         String arg = "New test Action";
         String newArg = "Testing new args";
 
